dbcbef865e6c20a3c4e8eaf0482ef4d450566ad0
Daniel Nilsson-Cole <dev.baiisdata@gmail.com>
2023-01-10 08:35:28 -0500

    _validate_env_variables_for_pg_conn()

--- baiis_utils/connectors/db/postgres/__init__.py
+++ baiis_utils/connectors/db/postgres/__init__.py
@@ -11,16 +11,31 @@ from psycopg2.extensions import connection
 
 def get_postgres_conn_from_env(db_code: str) -> connection:
 
+    _validate_env_variables_for_pg_conn(db_code)
+
     database: str = os.getenv(f'{db_code}_DB')
     user: str = os.getenv(f'{db_code}_USER')
     password: str = os.getenv(f'{db_code}_PASSWORD')
     host: str = os.getenv(f'{db_code}_URL')
 
+    conn: connection = pg.connect(
+        database=database,
+        user=user,
+        password=password,
+        host=host
+    )
+    conn.autocommit = True
+
+    return conn 
+
+
+def _validate_env_variables_for_pg_conn(db_code: str) -> None:
+
     env_variables: Dict[str,str] = {
-            f'{db_code}_DB': database,
-            f'{db_code}_USER': user,
-            f'{db_code}_PASSWORD': password,
-            f'{db_code}_URL': host
+            f'{db_code}_DB': os.getenv(f'{db_code}_DB'),
+            f'{db_code}_USER': os.getenv(f'{db_code}_USER'),
+            f'{db_code}_PASSWORD': os.getenv(f'{db_code}_PASSWORD'),
+            f'{db_code}_URL': os.getenv(f'{db_code}_URL')
         }
 
     missing_env_vars: List[str] = []
@@ -32,13 +47,3 @@ def get_postgres_conn_from_env(db_code: str) -> connection:
     if missing_env_vars:
         error_msg: str = f'  - the following environment variables need to be set:\n {missing_env_vars}'
         raise Exception(error_msg) 
-
-    conn: connection = pg.connect(
-        database=database,
-        user=user,
-        password=password,
-        host=host
-    )
-    conn.autocommit = True
-
-    return conn 
